0366e2875c4d0d61e86f1a92b05c8799bc17af9c
Danones <diogo.sousa.uk@gmail.com>
2020-11-26 15:12:41 +0000

    Code clean up

--- script.js
+++ script.js
@@ -14,25 +14,19 @@ const scoreComputer = document.querySelector(".computer-score")
 const message = document.querySelector(".message");
 
 function game(playerSelection){
-    console.log("inside game");
+
     let roundResult = playRound(playerSelection, computerSelection);
 
-    console.log("roundResult: " + roundResult);
-    console.log("totalRounds: " + totalRounds);
-    console.log("maxRounds: " + maxRounds);
     if (totalRounds <= maxRounds){
-        console.log("inside while");
-        console.log("roundResult: " + roundResult);
+
         if(roundResult === playerWinner){
-            console.log("inside first if");
             playerScore++;
             totalRounds++;
         }else if (roundResult === computerWinner){
-            console.log("inside else");
+    
             computerScore++;
             totalRounds++;
         }else{
-            console.log("TIE")
             totalRounds++;
         }
     
@@ -41,12 +35,6 @@ function game(playerSelection){
         console.log("playerScore: " + playerScore);
         scoreComputer.textContent = computerScore;
         message.textContent = roundResult;
-
-
-    }else{
-        console.log("inside ELSE")
-        
-
     }
 
 }
@@ -54,8 +42,6 @@ function game(playerSelection){
 function playRound(playerSelection, computerSelection){
     
     computerSelection = computerPlay();
-    console.log("Computer Selection: " + " " + computerSelection)
-    console.log("Player Selection: " + " "  + playerSelection);
     let answer = "";
 
     if(playerSelection == computerSelection){
@@ -66,23 +52,19 @@ function playRound(playerSelection, computerSelection){
     }
 
     answer = playerWinner;
-    console.log(playerWinner);
-    console.log(playerSelection);
+
     switch(playerSelection){
         case "rock":
-            console.log("inside Rock case");
-            if(computerSelection == "paper"){
+            if(computerSelection === "paper"){
                 answer = computerWinner;
             }
             break;
         case "paper":
-            console.log("inside paper case");
             if(computerSelection === "scissor"){
                 answer = computerWinner;
             }
             break;
         case "scissor":
-            console.log("insisde scissor case");
             if(computerSelection === "rock"){
                 answer = computerWinner;
             }
@@ -94,13 +76,10 @@ console.log(answer);
 
 function computerPlay(){
 
-    console.log("inside computer play")
-
     return playOptions[(Math.round(Math.random() * 2 ))];
 }
 
 function displayResults(text){
-    console.log("inside displayResults")
     
     message.textContent = text;
 
@@ -115,15 +94,12 @@ function declareWinner(playerScore, computerScore){
 
 buttons.forEach((button) =>{
     button.addEventListener("click",() =>{
-        console.log("inside button")
         
         const rps = button.querySelector("p");
         playerSelection = rps.attributes.alt.value;
-
-        console.log("button pressed " +   playerSelection)
         
         if(totalRounds > maxRounds){
-            console.log("calling declareWinner")
+
             declareWinner(playerScore, computerScore);
             return;
         }
